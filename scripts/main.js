186c9d5c978b3917f2dcce700aeb45d91be195ab
Eugenius <smirnov.evgeniy789@gmail.com>
2026-08-20 20:59:53 +0500

    Implement loading screen with status updates and progress bar

---
+++ scripts/main.js
@@ -0,0 +1,55 @@
+// Loading Screen Script
+
+const overlay = document.querySelector('.overlay');
+const progressIndicator = document.querySelector('.progress-indicator');
+const statusWrapper = document.querySelector('#status-wrapper');
+const statusesBelt = document.querySelector('#statuses-belt');
+
+const statuses = [
+  'Стаскиваю одеялко',
+  'Делаю зарядку',
+  'Наливаю чаёк',
+  'Включаю мак',
+  'Дизайню кодом'
+];
+
+const statusesCount = statuses.length;
+
+for (let i=0; i < statusesCount; i++) {
+ let statusTitle = document.createElement('h1')
+ statusTitle.textContent=`${statuses[i]}`
+ statusesBelt.append(statusTitle)
+
+ let statusStyle = getComputedStyle(statusTitle);
+ let statusFullHeight = statusTitle.offsetHeight + parseFloat(statusStyle.marginTop) + parseFloat(statusStyle.marginBottom);
+ statusWrapper.style.height = statusFullHeight + 'px';
+};
+
+const minSwapTime = 500;
+const maxSwapTime = 800;
+let currentStatusIndex = 0;
+
+const getSwapTime = function() {
+  let progress = (currentStatusIndex - 1) / (statusesCount-1);
+  return Math.max(minSwapTime, maxSwapTime - (maxSwapTime - minSwapTime) * progress);
+};
+
+const showNextStatus = function() {
+  statusesBelt.style.transform = `translateY(${-100 * currentStatusIndex}%)`;
+  currentStatusIndex += 1;
+  
+  let progress = (currentStatusIndex / statusesCount) * 100;
+  progressIndicator.style.width = `${progress}%`;
+  
+  if (currentStatusIndex === statusesCount) {
+    setTimeout(function() {
+      overlay.style.transform = 'translateY(-100%)';
+    }, minSwapTime);
+    return;
+  }
+  setTimeout(showNextStatus, getSwapTime());
+};
+
+window.addEventListener('load', function(){
+  showNextStatus();
+});
